2a9bd7103773c540bca56be9d9d2ef58009a3ad2
Phuc <phucbopbnation@gmail.com>
2023-05-12 11:43:04 +0700

    path not fix

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-//import Eletronic from './src/screens/Eletronic'
-import Brand from '../src/screens'
+import Electronic from '../src/screens/Electronic'
+import Brand from '../src/screens/Brand'
 import Control from '../src/screens/Control'
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes () {
+  const theme = useContext(ThemeContext)
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Eletronic'>
-        <Stack.Screen name='Brand' component={Brand} />
+      <Stack.Navigator
+        initialRouteName='Electronic'
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerTintColor: theme.colors.text,
+          headerStyle: {
+            elevation: 0,
+            backgroundColor: theme.background
+          }
+        }}
+      >
+        <Stack.Screen
+          name='Electronic'
+          component={Electronic}
+          options={{ title: 'Electronic' }}
+        />
+        <Stack.Screen
+          name='Brand'
+          component={Brand}
+          options={{ title: 'Brand' }}
+        />
         <Stack.Screen name='Control' component={Control} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/screens/Electronic/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Background from '../src/components/Background'
+
+// import { Container } from './styled';
+
+export default function Electronic () {
+  return (
+    <Background />
+  )
+}
