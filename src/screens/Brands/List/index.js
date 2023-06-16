67eaab1d531b40dcbc59517c425fe367f7012f67
Phuc <phucbopbnation@gmail.com>
2023-06-16 22:52:20 +0700

    ..

--- src/screens/Brands/List/index.js
+++ src/screens/Brands/List/index.js
@@ -3,6 +3,8 @@ import t from 'prop-types'
 
 import Ripple from 'C:/Users/84373/Videos/Nodejs/ReactNative2023/rmt/src/components/Ripple'
 
+//import Ripple from '../../components/Ripple'
+
 import { Container, Content, Name } from './styled'
 
 const data = [
